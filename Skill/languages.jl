4837f3880c2603a7a40c6b1637541aaa5fe7bf67
andreas.dominik <andreas.dominik@mni.thm.de>
2020-03-18 10:19:17 +0100

    language support for en added

--- Skill/languages.jl
+++ Skill/languages.jl
@@ -2,6 +2,7 @@ Snips.addText("de", :dunno, "Ich weiß nicht was ich machen soll")
 Snips.addText("de", :no_sounds, "Ich kann keinen Weckerton in der config-Datei finden!")
 Snips.addText("de", :no_sound_file, "Die Datei mit dem Klingelton existiert nicht")
 Snips.addText("de", :sound_set, "Der Klingelton ist auf neu eingestellt und ist jetzt")
+Snips.addText("de", :with, "mit")
 
 Snips.addText("de", :wakeup_scheduled, "Der Wecker ist eingestellt auf")
 Snips.addText("de", :deleted, "Der Wecker ist ausgeschaltet!")
@@ -11,6 +12,18 @@ Snips.addText("de", :good_morning, "Guten morgen, es ist")
 Snips.addText("de", :good_morning, "Guten morgen, heute ist")
 Snips.addText("de", :good_morning, "Guten morgen, am")
 
+Snips.addText("en", :dunno, "I don't know what to do")
+Snips.addText("en", :no_sounds, "I am not able to find any ringtones in the config file!")
+Snips.addText("en", :no_sound_file, "I'm unable to find the file with the requested ringtone")
+Snips.addText("en", :sound_set, "The new ringtone is")
+Snips.addText("en", :with, "with")
+
+Snips.addText("en", :wakeup_scheduled, "The wakeup call ist scheduled for")
+Snips.addText("en", :deleted, "The wakeup call is deleted!")
+
+
+Snips.addText("en", :good_morning, "Good morning, it is ")
+Snips.addText("en", :good_morning, "Good morning, today is")
 
 # regex for time correction with input string:
 #
